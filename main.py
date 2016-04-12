04ea26164269edddde94c0bbb88fd3359328663e
Alexander Areshnikov <alex.areshnikov@plansource.com>
2016-04-12 11:55:45 -0600

    Refactor: get rid of redundant thread

--- main.py
+++ main.py
@@ -7,36 +7,38 @@ import json
 
 CAR_NAME = 'carName'
 
-class Listener(threading.Thread):
-    def __init__(self, vehicle):
-        threading.Thread.__init__(self)
-
-        self.vehicle = vehicle
+class Manager:
+    def __init__(self, name):
+        self.vehicle = Vehicle(name)
 
         redis_config = yaml.safe_load(open('redis-conf.yml'))
         print redis_config
 
         self.redis = redis.Redis(host=redis_config['host'], port=redis_config['port'])
         self.pubsub = self.redis.pubsub()
-        self.pubsub.subscribe(vehicle.name)
+        self.pubsub.subscribe(self.vehicle.name)
 
-    def run(self):
+    def execute(self):
         for item in self.pubsub.listen():
             if item['data'] == 'KILL':
-                self.pubsub.unsubscribe()
-                self.vehicle.shutdown()
-                print self, 'unsubscribed and finished'
+                self.shutdown()
                 break
             elif isinstance(item['data'], str):
                 try:
-                    #print self, "ITEM DATA " + item['data']
                     self.vehicle.update(json.loads(item['data']))
-                    #print self, "-----------------"
                 except ValueError:
-                    print self, 'ERROR: Bad JSON received: ' + item['data']
+                    print 'ERROR: Bad JSON received: ' + item['data']
             else:
-                print self, 'Non string value received. Skipped.'
+                print 'Non string value received. Skipped.'
+
+    def shutdown(self):
+        self.pubsub.unsubscribe()
+        self.vehicle.shutdown()
+        print 'unsubscribed and finished'
 
 if __name__ == "__main__":
-    client = Listener(Vehicle(CAR_NAME))
-    client.start()
+    try:
+        manager = Manager(CAR_NAME)
+        manager.execute()
+    except KeyboardInterrupt:
+        manager.shutdown()
