672ac11ca45b86d6f844d731f30ab4333a426a9d
Alexander Areshnikov <alex.areshnikov@plansource.com>
2016-04-21 18:34:27 -0600

    Implement TCP sockets

--- main.py
+++ main.py
@@ -3,47 +3,59 @@ from lib.vehicle import Vehicle
 import redis
 import yaml
 import json
+import socket
 
 class Listener:
+    BUFSIZ = 1024
+
     def __init__(self):
-        config = yaml.safe_load(open('config.yml'))
+        self.config = yaml.safe_load(open('config.yml'))
 
-        self.vehicle = Vehicle(config)
-        self.initialize_redis()
+        self.vehicle = Vehicle(self.config)
+        self.tcp_cli_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def initialize_redis(self):
-        redis_config = yaml.safe_load(open('redis-conf.yml'))
-        redis_config.setdefault('password', None)
-
-        self.redis = redis.Redis(
-            host=redis_config['host'],
-            port=redis_config['port'],
-            password=redis_config['password'])
-
-        self.redis.setex('cars-list-refresh', '', 1)
-        self.redis.execute_command('client', 'setname', '__vehicle__' + self.vehicle.name)
-
-        self.pubsub = self.redis.pubsub()
-        self.pubsub.subscribe(self.vehicle.name)
+    # def initialize_redis(self):
+    #     redis_config = yaml.safe_load(open('redis-conf.yml'))
+    #     redis_config.setdefault('password', None)
+    #
+    #     self.redis = redis.Redis(
+    #         host=redis_config['host'],
+    #         port=redis_config['port'],
+    #         password=redis_config['password'])
+    #
+    #     self.redis.setex('cars-list-refresh', '', 1)
+    #     self.redis.execute_command('client', 'setname', '__vehicle__' + self.vehicle.name)
+    #
+    #     self.pubsub = self.redis.pubsub()
+    #     self.pubsub.subscribe(self.vehicle.name)
 
     def listen(self):
-        for item in self.pubsub.listen():
-            if item['data'] == 'KILL':
+        self.tcp_cli_sock.connect((self.config['host'], self.config['port']))
+        print 'Connected'
+
+        self.tcp_cli_sock.send('vn:' + self.vehicle.name)
+
+        while True:
+            data = self.tcp_cli_sock.recv(self.BUFSIZ)    # Receive data sent from the client.
+            print data
+
+            if not data:
+                print 'Server has gone away.'
+                break
+            elif data == 'KILL':
                 self.shutdown()
                 break
-            elif isinstance(item['data'], str):
+            elif isinstance(data, str):
                 try:
-                    self.vehicle.update(json.loads(item['data']))
+                    self.vehicle.update(json.loads(data))
                 except ValueError:
-                    print 'ERROR: Bad JSON received: ' + item['data']
+                    print 'ERROR: Bad JSON received: ' + data
             else:
                 print 'Non string value received. Skipped.'
 
     def shutdown(self):
-        self.pubsub.unsubscribe()
         self.vehicle.shutdown()
-        self.redis.setex('cars-list-refresh', '', 1)
-        print 'unsubscribed and finished'
+        print 'Finished'
 
 if __name__ == "__main__":
     try:
